767aa8a229b0a7956a4b9d676eca538a75fa272f
KyleCSharp <kylelrigler@gmail.com>
2022-09-17 14:43:19 -0400

    done

---
+++ EncapsulationEx1/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationEx1
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+        private double Balance = 0;
+
+       
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ EncapsulationEx1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EncapsulationEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("your new bank account has been set up, and has a balance of 0");
+            Console.WriteLine("how much would you like to deposit");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"The new account balance is...${account.GetBalance()}");
+           
+            Console.WriteLine("Thank you!");
+
+
+        }
+    }
+}
